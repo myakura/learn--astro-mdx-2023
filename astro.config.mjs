bea4423644363d3f3b75094c83a0c8b0c557af16
Masataka Yakura <masataka.yakura@gmail.com>
2023-02-19 02:41:22 +0900

    apply remark-sectionize to markdown pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import mdx from '@astrojs/mdx';
 import sectionize from 'remark-sectionize';
 
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [sectionize],
+	},
 	integrations: [
 		mdx({
 			remarkPlugins: [sectionize],
