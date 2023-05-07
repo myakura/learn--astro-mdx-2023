9d7b839302a6de8a2ed8449a8ac6007894f0635d
Masataka Yakura <masataka.yakura@gmail.com>
2023-05-08 03:52:00 +0900

    add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sectionize from 'remark-sectionize';
 
@@ -6,5 +7,5 @@ export default defineConfig({
 	markdown: {
 		remarkPlugins: [sectionize],
 	},
-	integrations: [mdx()],
+	integrations: [react(), mdx()],
 });
